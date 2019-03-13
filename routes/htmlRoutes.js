4549a38e8dec5f6a2eac01b74022561d236b88e3
Princess Wilson <aurorapwilson1@gmail.com>
2019-03-13 19:51:27 -0400

    working on routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -29,7 +29,7 @@ module.exports = function(app) {
         res.render("404");
     });
     //Load main.html
-    app.get('/ ', function(req, res) {
+    app.get('/', function(req, res) {
         res.sendFile(path.join(__dirname, "./layouts/main.html"));
     });
 
--- slack.js
+++ slack.js
@@ -110,23 +110,23 @@ function updateUsersInRoom(namespace, roomToJoin) {
     })
 }
 
-var syncOptions = { force: false };
+// var syncOptions = { force: false };
 
-// If running a test, set syncOptions.force to true
-// clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
-    syncOptions.force = true;
-}
+// // If running a test, set syncOptions.force to true
+// // clearing the `testdb`
+// if (process.env.NODE_ENV === "test") {
+//     syncOptions.force = true;
+// }
 
 
-db.sequelize.sync(syncOptions).then(function() {
-    app.listen(PORT, function() {
-        console.log(
-            "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-            PORT,
-            PORT
-        );
-    });
-});
+// db.sequelize.sync(syncOptions).then(function() {
+//     app.listen(PORT, function() {
+//         console.log(
+//             "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+//             PORT,
+//             PORT
+//         );
+//     });
+// });
 
 module.exports = app;
