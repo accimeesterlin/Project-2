d25b261d977dcc8b633337ddc7e3be5429a0ea74
Joel Brown <joelbrown90@gmail.com>
2019-03-16 01:32:32 -0400

    package

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,94 +1,80 @@
-var db = require("../models");
+// var db = require("../models");
 
-<<<<<<< HEAD
-module.exports = function(app) {
-    // Load index page
-    // app.get("/", function(req, res) {
-    //     db.Example.findAll({}).then(function(dbExamples) {
-    //         res.render("index", {
-    //             msg: "Welcome!",
-    //             examples: dbExamples
-    //         });
-    //     });
-    // });
+module.exports = function(app, path) {
 
-    // // Load example page and pass in an example by id
-    // app.get("/example/:id", function(req, res) {
-    //     db.Example.findOne({
-    //         where: {
-    //             id: req.params.id
-    //         }
-    //     }).then(function(dbExample) {
-    //         res.render("example", {
-    //             example: dbExample
-    //         });
-    //     });
-    // });
+
+    app.get('/', function(req, res) {
+        res.sendFile(path.join(__dirname, "../views/layouts/main.html"));
+    });
+    app.get('/login', function(req, res) {
+        res.sendFile(path.join(__dirname, "../views/login-page.html"));
+    });
+
+    app.get('/signup', function(req, res) {
+        res.sendFile(path.join(__dirname, "../views/signup-page.html"));
+    });
+    app.get('/dashboard', function(req, res) {
+        res.sendFile(path.join(__dirname, "../views/chat.html"));
+    });
+
+    // app.use(function(req, res, next){
+    //   res.status(404);
 
     // Render 404 page for any unmatched routes
-    app.get("*", function(req, res) {
-        res.render("404");
-    });
-    //Load main.html
-    app.get('/', function(req, res) {
-        res.sendFile(path.join(__dirname, "../login-page.html"));
-    });
+    //  app.get("*", function (req, res) {
+    //   res.render("404");
+    // });
+    // };
 
-    //Load login-page.html
-    app.get('/login', function(req, res) {
-        res.sendFile(path.join(__dirname, "./login-page.html"));
-    });
+    //   // respond with html page
+    //   if (req.accepts('html')) {
+    //     //TODO: Replace the signup-page.html to 4040.html or something similar
+    //     res.sendFile(path.join(__dirname,"../views/"));
+    //     return;
+    //   }
+    // });
 
-    //Load signup-page.html
-    app.get('/signup', function(req, res) {
-        res.sendFile(path.join(__dirname, "./signup-page.html"));
-    });
+    // Load index page
+    //   app.get("/", function (req, res) {
+    //     db.Example.findAll({}).then(function (dbExamples) {
+    //       res.render("index", {
+    //         msg: "Welcome!",
+    //         examples: dbExamples
+    //       });
+    //     });
+    //   });
 
-};
-=======
-module.exports = function (app) {
-  // Load index page
-  app.get("/", function (req, res) {
-    db.Example.findAll({}).then(function (dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
-  });
+    //   // Load example page and pass in an example by id
+    //   app.get("/example/:id", function (req, res) {
+    //     db.Example.findOne({
+    //       where: {
+    //         id: req.params.id
+    //       }
+    //     }).then(function (dbExample) {
+    //       res.render("example", {
+    //         example: dbExample
+    //       });
+    //     });
+    //   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function (req, res) {
-    db.Example.findOne({
-      where: {
-        id: req.params.id
-      }
-    }).then(function (dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
+    // // Render 404 page for any unmatched routes
+    // app.get("*", function (req, res) {
+    //   res.render("404");
+    // });
+    // };
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function (req, res) {
-    res.render("404");
-  });
-  // };
+    //   //Load main.html
+    // app.get('/ ', function(req, res) {
+    //   res.sendFile(path.join(__dirname, "../views/layouts/main.html"));
+    // });
 
-  //Load main.html
-  app.get('/ ', function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/layouts/main.html"));
-  });
+    // //Load login-page.html
+    // app.get('/login', function(req, res) {
+    // res.sendFile(path.join(__dirname, "../views/login-page.html"));
+    // });
 
-//Load login-page.html
-app.get('/login', function(req, res) {
-res.sendFile(path.join(__dirname, "../views/login-page.html"));
-});
-
-//Load signup-page.html
-app.get('/signup', function(req, res) {
-res.sendFile(path.join(__dirname, "../views/signup-page.html"));
-});
+    // //Load signup-page.html
+    // app.get('/signup', function(req, res) {
+    // res.sendFile(path.join(__dirname, "../views/signup-page.html"));
+    // });
 }
->>>>>>> origin
