22809c5b840e404242874b7a879c9d9b2fcb51b5
Princess Wilson <aurorapwilson1@gmail.com>
2019-03-15 23:45:18 -0400

    html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,39 @@
 var db = require("../models");
 
 module.exports = function (app, path) {
+  
+
+    app.get('/', function (req, res) {
+      res.sendFile(path.join(__dirname,"../views/layouts/main.html"));
+    });
     app.get('/login', function (req, res) {
       res.sendFile(path.join(__dirname,"../views/login-page.html"));
     });
+
+    app.get('/signup', function (req, res) {
+      res.sendFile(path.join(__dirname,"../views/signup-page.html"));
+    });
+    app.get('/dashboard', function (req, res) {
+      res.sendFile(path.join(__dirname,"../views/chat.html"));
+    });
+    
+    // app.use(function(req, res, next){
+    //   res.status(404);
+
+       // Render 404 page for any unmatched routes
+      //  app.get("*", function (req, res) {
+      //   res.render("404");
+      // });
+      // };
+    
+    //   // respond with html page
+    //   if (req.accepts('html')) {
+    //     //TODO: Replace the signup-page.html to 4040.html or something similar
+    //     res.sendFile(path.join(__dirname,"../views/"));
+    //     return;
+    //   }
+    // });
+
     // Load index page
     //   app.get("/", function (req, res) {
     //     db.Example.findAll({}).then(function (dbExamples) {
@@ -27,16 +57,16 @@ module.exports = function (app, path) {
     //     });
     //   });
 
-    //   // Render 404 page for any unmatched routes
-    //   app.get("*", function (req, res) {
-    //     res.render("404");
-    //   });
-    //   // };
+      // // Render 404 page for any unmatched routes
+      // app.get("*", function (req, res) {
+      //   res.render("404");
+      // });
+      // };
 
     //   //Load main.html
-    //   app.get('/ ', function(req, res) {
-    //     res.sendFile(path.join(__dirname, "../views/layouts/main.html"));
-    //   });
+      // app.get('/ ', function(req, res) {
+      //   res.sendFile(path.join(__dirname, "../views/layouts/main.html"));
+      // });
 
     // //Load login-page.html
     // app.get('/login', function(req, res) {
